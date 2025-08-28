f66adc27fa4bbb53cba61c4a8eea5f3918d4b0ae
RealFilllykung <filllykunggm@gmail.com>
2025-08-28 20:17:49 +0700

    feat: additional exports

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
 const { toggleStringInput } = require('./utils/text-toggler');
+const { isThaiCharacter } = require('./utils/thai-character-checker');
 
-module.exports = { toggleStringInput };
+module.exports = { 
+    toggleStringInput,
+    isThaiCharacter
+};
